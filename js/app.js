1ea81186232328423fb42f35c98e2f44fffd2db4
Ezequiel Lino <sonisombra@gmail.com>
2023-04-26 10:39:35 -0600

    update

--- js/app.js
+++ js/app.js
@@ -39,3 +39,23 @@ function eventListeners() {
   const formulario = document.querySelector('#cotizar-seguro');
   formulario.addEventListener('submit', cotizarSeguro);
 }
+
+function cotizarSeguro(e) {
+  e.preventDefault();
+
+  // Leer la marca seleccionada
+  const marca = document.querySelector('#marca').value;
+
+  // Leer el año seleccionado
+  const year = document.querySelector('#year').value;
+
+  // Leemos el tipo de cobertura, de los radio button
+  const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+  if (marca === '' || year === '' || tipo === '') {
+    console.log('No paso la validacion');
+  } else {
+    console.log('Si paso');
+  }
+
+}
